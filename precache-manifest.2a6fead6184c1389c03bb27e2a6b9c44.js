825673c06e5dd13678fc420c6e03024dc33c5db7
brian <brian.oliveira100@gmail.com>
2020-03-30 17:37:41 -0400

    Updates

---
+++ precache-manifest.2a6fead6184c1389c03bb27e2a6b9c44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3218bd4a5a87419fbf94b73ce95eeb66",
+    "url": "/PomodoroTimer/index.html"
+  },
+  {
+    "revision": "b9cc90449c534328e7a9",
+    "url": "/PomodoroTimer/static/css/main.28f568b3.chunk.css"
+  },
+  {
+    "revision": "bb36b2bd56e6f65167e3",
+    "url": "/PomodoroTimer/static/js/2.ee29a942.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/PomodoroTimer/static/js/2.ee29a942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cc90449c534328e7a9",
+    "url": "/PomodoroTimer/static/js/main.53f5bacc.chunk.js"
+  },
+  {
+    "revision": "9bc17c6f1106c440aac8",
+    "url": "/PomodoroTimer/static/js/runtime-main.6e3f077e.js"
+  },
+  {
+    "revision": "7728da742f84654587a36f780adf9152",
+    "url": "/PomodoroTimer/static/media/backgroundPath.7728da74.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PomodoroTimer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62e76a53668c174b367ad45428ee8728",
+    "url": "/PomodoroTimer/static/media/logo.62e76a53.svg"
+  }
+]);
